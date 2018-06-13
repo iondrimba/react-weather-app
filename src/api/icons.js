a267b46092c54387cd1dafa9509bd8c78524e660
Ion D. Filho <iondrimba@gmail.com>
2018-06-12 21:12:08 -0300

    added svgs

---
+++ src/api/icons.js
@@ -0,0 +1,202 @@
+export default {
+  '1': {
+    day: true,
+    night: false,
+    text: 'sunny',
+  },
+  '2': {
+    day: true,
+    night: false,
+    text: 'mostly sunny',
+  },
+  '3': {
+    day: true,
+    night: false,
+    text: 'partly sunny',
+  },
+  '4': {
+    day: true,
+    night: false,
+    text: 'Intermittent Clouds',
+  },
+  '5': {
+    day: true,
+    night: false,
+    text: 'Hazy Sunshine',
+  },
+  '6': {
+    day: true,
+    night: false,
+    text: 'Mostly Cloudy',
+  },
+  '7': {
+    day: true,
+    night: true,
+    text: 'Cloudy',
+  },
+  '8': {
+    day: true,
+    night: true,
+    text: 'Dreary (Overcast)',
+  },
+  '11': {
+    day: true,
+    night: true,
+    text: 'Fog',
+  },
+  '12': {
+    day: true,
+    night: true,
+    text: 'Showers',
+  },
+  '13': {
+    day: true,
+    night: false,
+    text: 'Mostly Cloudy w/ Showers',
+  },
+  '14': {
+    day: true,
+    night: false,
+    text: 'Partly Sunny w/ Showers',
+  },
+  '15': {
+    day: true,
+    night: true,
+    text: 'T-Storms',
+  },
+  '16': {
+    day: true,
+    night: false,
+    text: 'Mostly Cloudy w/ T-Storms',
+  },
+  '17': {
+    day: true,
+    night: false,
+    text: 'Partly Sunny w/ T-Storms',
+  },
+  '18': {
+    day: true,
+    night: true,
+    text: 'Rain',
+  },
+  '19': {
+    day: true,
+    night: true,
+    text: 'Flurries',
+  },
+  '20': {
+    day: true,
+    night: false,
+    text: 'Mostly Cloudy w/ Flurries',
+  },
+  '21': {
+    day: true,
+    night: false,
+    text: 'Partly Sunny w/ Flurries',
+  },
+  '22': {
+    day: true,
+    night: true,
+    text: 'Snow',
+  },
+  '23': {
+    day: true,
+    night: false,
+    text: 'Mostly Cloudy w/ Snow',
+  },
+  '24': {
+    day: true,
+    night: true,
+    text: 'Ice',
+  },
+  '25': {
+    day: true,
+    night: true,
+    text: 'Sleet',
+  },
+  '26': {
+    day: true,
+    night: true,
+    text: 'Freezing Rain',
+  },
+  '29': {
+    day: true,
+    night: true,
+    text: 'Rain and Snow',
+  },
+  '30': {
+    day: true,
+    night: true,
+    text: 'Hot',
+  },
+  '31': {
+    day: true,
+    night: true,
+    text: 'Cold',
+  },
+  '32': {
+    day: true,
+    night: true,
+    text: 'Windy',
+  },
+  '33': {
+    day: false,
+    night: true,
+    text: 'Clear',
+  },
+  '34': {
+    day: false,
+    night: true,
+    text: 'Mostly Clear',
+  },
+  '35': {
+    day: false,
+    night: true,
+    text: 'Partly Cloudy',
+  },
+  '36': {
+    day: false,
+    night: true,
+    text: 'Intermittent Clouds',
+  },
+  '37': {
+    day: false,
+    night: true,
+    text: 'Hazy Moonlight',
+  },
+  '38': {
+    day: false,
+    night: true,
+    text: 'Mostly Cloudy',
+  },
+  '39': {
+    day: false,
+    night: true,
+    text: 'Partly Cloudy w/ Showers',
+  },
+  '40': {
+    day: false,
+    night: true,
+    text: 'Mostly Cloudy w/ Showers',
+  },
+  '41': {
+    day: false,
+    night: true,
+    text: 'Partly Cloudy w/ T-Storms',
+  },
+  '42': {
+    day: false,
+    night: true,
+    text: 'Mostly Cloudy w/ T-Storms',
+  },
+  '43': {
+    day: false,
+    night: true,
+    text: 'Mostly Cloudy w/ Flurries',
+  },
+  '44': {
+    day: false,
+    night: true,
+    text: 'Mostly Cloudy w/ Snow',
+  },
+}
