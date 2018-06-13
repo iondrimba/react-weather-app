908c6bc77f961bafac28b9338691aadc468b4fe8
Ion D. Filho <iondrimba@gmail.com>
2018-06-13 16:14:59 -0300

    added icon ids

--- src/api/icons.js
+++ src/api/icons.js
@@ -3,191 +3,229 @@ export default {
     day: true,
     night: false,
     text: 'sunny',
+    id: 'wi-day-sunny'
   },
   '2': {
     day: true,
     night: false,
     text: 'mostly sunny',
+    id: 'wi-day-cloudy'
   },
   '3': {
     day: true,
     night: false,
     text: 'partly sunny',
+    id: 'wi-day-cloudy'
   },
   '4': {
     day: true,
     night: false,
     text: 'Intermittent Clouds',
+    id: 'wi-day-cloudy'
   },
   '5': {
     day: true,
     night: false,
     text: 'Hazy Sunshine',
+    id: 'wi-day-haze'
   },
   '6': {
     day: true,
     night: false,
     text: 'Mostly Cloudy',
+    id: 'wi-day-cloudy-high'
   },
   '7': {
     day: true,
     night: true,
     text: 'Cloudy',
+    id: 'wi-cloudy'
   },
   '8': {
     day: true,
     night: true,
     text: 'Dreary (Overcast)',
+    id: 'wi-day-sunny-overcast'
   },
   '11': {
     day: true,
     night: true,
     text: 'Fog',
+    id: 'wi-fog'
   },
   '12': {
     day: true,
     night: true,
     text: 'Showers',
+    id: 'wi-showers'
   },
   '13': {
     day: true,
     night: false,
     text: 'Mostly Cloudy w/ Showers',
+    id: 'wi-day-showers'
   },
   '14': {
     day: true,
     night: false,
     text: 'Partly Sunny w/ Showers',
+    id: 'wi-day-sleet'
   },
   '15': {
     day: true,
     night: true,
     text: 'T-Storms',
+    id: 'wi-storm-showers'
   },
   '16': {
     day: true,
     night: false,
     text: 'Mostly Cloudy w/ T-Storms',
+    id: 'wi-storm-showers'
   },
   '17': {
     day: true,
     night: false,
     text: 'Partly Sunny w/ T-Storms',
+    id: 'wi-day-storm-showers'
   },
   '18': {
     day: true,
     night: true,
     text: 'Rain',
+    id: 'wi-rain'
   },
   '19': {
     day: true,
     night: true,
     text: 'Flurries',
+    id: 'wi-cloudy'
   },
   '20': {
     day: true,
     night: false,
     text: 'Mostly Cloudy w/ Flurries',
+    id: 'wi-day-snow'
   },
   '21': {
     day: true,
     night: false,
     text: 'Partly Sunny w/ Flurries',
+    id: 'wi-day-snow-wind'
   },
   '22': {
     day: true,
     night: true,
     text: 'Snow',
+    id: 'wi-snow'
   },
   '23': {
     day: true,
     night: false,
     text: 'Mostly Cloudy w/ Snow',
+    id: 'wi-day-snow-wind'
   },
   '24': {
     day: true,
     night: true,
     text: 'Ice',
+    id: 'wi-snowflake-cold'
   },
   '25': {
     day: true,
     night: true,
     text: 'Sleet',
+    id: 'wi-sleet'
   },
   '26': {
     day: true,
     night: true,
     text: 'Freezing Rain',
+    id: 'wi-rain-mix'
   },
   '29': {
     day: true,
     night: true,
     text: 'Rain and Snow',
+    id: 'wi-rain-mix'
   },
   '30': {
     day: true,
     night: true,
     text: 'Hot',
+    id: 'wi-hot'
   },
   '31': {
     day: true,
     night: true,
     text: 'Cold',
+    id: 'wi-snowflake-cold'
   },
   '32': {
     day: true,
     night: true,
     text: 'Windy',
+    id: 'wi-windy'
   },
   '33': {
     day: false,
     night: true,
     text: 'Clear',
+    id: 'wi-night-clear'
   },
   '34': {
     day: false,
     night: true,
     text: 'Mostly Clear',
+    id: 'wi-night-alt-partly-cloudy'
   },
   '35': {
     day: false,
     night: true,
     text: 'Partly Cloudy',
+    id: 'wi-night-partly-cloudy'
   },
   '36': {
     day: false,
     night: true,
     text: 'Intermittent Clouds',
+    id: 'wi-night-alt-cloudy-high'
   },
   '37': {
     day: false,
     night: true,
     text: 'Hazy Moonlight',
+    id: 'wi-night-cloudy-high'
   },
   '38': {
     day: false,
     night: true,
     text: 'Mostly Cloudy',
+    id: 'wi-night-cloudy'
   },
   '39': {
     day: false,
     night: true,
     text: 'Partly Cloudy w/ Showers',
+    id: 'wi-night-alt-showers'
   },
   '40': {
     day: false,
     night: true,
     text: 'Mostly Cloudy w/ Showers',
+    id: 'wi-night-alt-showers'
   },
   '41': {
     day: false,
     night: true,
     text: 'Partly Cloudy w/ T-Storms',
+    id: 'wi-night-alt-storm-showers'
   },
   '42': {
     day: false,
     night: true,
     text: 'Mostly Cloudy w/ T-Storms',
+    id: 'wi-night-alt-storm-showers'
   },
   '43': {
     day: false,
@@ -198,5 +236,6 @@ export default {
     day: false,
     night: true,
     text: 'Mostly Cloudy w/ Snow',
+    id: 'wi-night-alt-snow'
   },
 }
