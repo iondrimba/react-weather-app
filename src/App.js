d796146eac9363e9863974ab38d8407989232bae
Ion D. Filho <iondrimba@gmail.com>
2018-06-13 19:33:22 -0300

    moved folder

--- src/App.js
+++ src/App.js
@@ -83,27 +83,32 @@ class App extends Component {
         {
           time: 8,
           rainProbability: 20,
-          temperature: 23
+          temperature: 23,
+          icon: 'wi-fog'
         },
         {
           time: 9,
           rainProbability: 22,
-          temperature: 25
+          temperature: 25,
+          icon: 'wi-day-sunny'
         },
         {
           time: 10,
           rainProbability: 40,
-          temperature: 15
+          temperature: 15,
+          icon: 'wi-showers'
         },
         {
           time: 11,
           rainProbability: 15,
-          temperature: 16
+          temperature: 16,
+          icon: 'wi-day-snow-wind'
         },
         {
           time: 12,
           rainProbability: 5,
-          temperature: 24
+          temperature: 24,
+          icon: 'wi-night-alt-showers'
         }
       ]
     };
@@ -133,8 +138,10 @@ class App extends Component {
                 <span className="temperature__unit-letter">c</span>
               </div>
             </div>
+            <div className="current-condition__details">
             <div className="wind">
               <span className="wind__label">wind</span>
+              <div>
               <div className="wind__speed">
                 <span className="wind__speed-icon">
                     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
@@ -168,6 +175,7 @@ class App extends Component {
                 </span>
                 <span className="wind__direction-value">{this.state.currentCondition.wind.direction}</span>
               </div>
+              </div>
             </div>
             <div className="uv">
               <span className="uv__label">uv</span>
@@ -176,8 +184,35 @@ class App extends Component {
                 <span className="uv__text">{this.state.currentCondition.uv.description}</span>
               </div>
             </div>
-          </div>
+            </div>
+              </div>
         </section>
+        <section className="forecast-hourly">
+          {
+            this.state.foreCastHourly.map((item) => {
+              return <div className="forecast-hourly__item">
+                <span className="forecast-hourly__item-hour">{`${item.time}:00`}</span>
+                <img alt="icon" src={`svg/${item.icon}.svg`}/>
+                <p className="forecast-hourly__item-temperature">{item.temperature}
+                  <span className="temperature__unit-dot"></span>
+                </p>
+                <div className="rain">
+                  <span className="rain__icon">
+                  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                      viewBox="0 0 30 30" Style="enable-background:new 0 0 30 30;" >
+                    <path d="M9.81,15.25c0,0.92,0.23,1.78,0.7,2.57s1.1,1.43,1.9,1.9c0.8,0.47,1.66,0.71,2.59,0.71c0.93,0,1.8-0.24,2.61-0.71
+                      c0.81-0.47,1.45-1.11,1.92-1.9c0.47-0.8,0.71-1.65,0.71-2.57c0-0.6-0.17-1.31-0.52-2.14c-0.35-0.83-0.77-1.6-1.26-2.3
+                      c-0.44-0.57-0.96-1.2-1.56-1.88c-0.6-0.68-1.65-1.73-1.89-1.97l-1.28,1.29c-0.62,0.6-1.22,1.29-1.79,2.08
+                      c-0.57,0.79-1.07,1.64-1.49,2.55C10.01,13.79,9.81,14.58,9.81,15.25z"/>
+                    </svg>
+                  </span>
+                  <span className="rain__probability">{`${item.rainProbability}%`}</span>
+                </div>
+              </div>
+            })
+          }
+        </section>
+
         <section className="forecast-daily">
           {
             this.state.foreCastDaily.map((item) => {
@@ -191,17 +226,7 @@ class App extends Component {
           }
         </section>
 
-        <section className="forecast-hourly">
-          {
-            this.state.foreCastHourly.map((item) => {
-              return <div className="forecast-hourly__item">
-                <span>{item.time}</span>
-                <span>{item.rainProbability}</span>
-                <span>{item.temperature}</span>
-              </div>
-            })
-          }
-        </section>
+
       </div>
     );
   }
