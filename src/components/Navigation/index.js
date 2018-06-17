39d2cb37d7be8fa089d92e64322fd13f7cb68938
Ion D. Filho <iondrimba@gmail.com>
2018-06-17 18:15:08 -0300

    extracted navigation

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <section className="navigation">
+        <div className="navigation-dot"></div>
+        <div className="navigation-dot"></div>
+      </section>
+    )
+  }
+}
+
+export default Navigation;
+
+
