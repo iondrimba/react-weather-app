b148a695ba4ec0aa91f75e5dcccac64337ba8814
Ion D. Filho <iondrimba@gmail.com>
2018-06-17 17:53:41 -0300

    extracted location section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import IpSearch from './api/ipSearch';
 import CurrentCondition from './api/currentCondition';
 import ForeCast from './api/foreCast';
 import initialState from './initialState';
+import Location from './components/Location';
 
 class App extends Component {
   constructor() {
@@ -32,14 +33,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <section className="location">
-            <span className="location__icon">
-              <svg enable-background="new 0 0 24 24" height="24px" id="Layer_1" version="1.1" viewBox="0 0 24 24" width="24px">
-                <path d="M11.513,12.38c-2.117,0-3.835-1.729-3.835-3.862c0-2.135,1.718-3.863,3.835-3.863s3.835,1.729,3.835,3.863  C15.348,10.65,13.63,12.38,11.513,12.38 M11.513,0C6.825,0,3.025,3.827,3.025,8.549c0,4.46,3.844,10.213,6.411,13.014  c0.959,1.045,2.076,2.454,2.076,2.454s1.2-1.417,2.229-2.493C16.306,18.84,20,13.451,20,8.549C20,3.827,16.2,0,11.513,0"/>
-              </svg>
-            </span>
-            <span className="location__text">{this.state.currentCondition.location}</span>
-          </section>
+        <Location location={this.state.currentCondition.location} />
         <section className="current-condition">
           <div className="wrapper-temperature">
           <span className="temperature__status">{this.state.currentCondition.weather}</span>
