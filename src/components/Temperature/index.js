75cb587629c53869bb69812ed13247a76f544220
Ion D. Filho <iondrimba@gmail.com>
2018-06-19 20:17:22 -0300

    refactored animations

--- src/components/Temperature/index.js
+++ src/components/Temperature/index.js
@@ -1,14 +1,20 @@
-
 import React, { Component } from 'react';
+import rAFTimeout from '../../helpers/rAFTimeout';
 import './index.css';
 
 class Temperature extends Component {
+  animate() {
+    rAFTimeout(() => this.status.classList.add('animate-in'), 250);
+    rAFTimeout(() => this.value.classList.add('animate-in'), 350);
+    rAFTimeout(() => this.unit.classList.add('animate-in'), 400);
+  }
+
   componentDidMount() {
-    requestAnimationFrame(() => {
-      document.querySelector('.temperature__status').classList.add('animate-in');
-      document.querySelector('.temperature__value').classList.add('animate-in');
-      document.querySelector('.temperature__unit').classList.add('animate-in');
-    });
+    this.status = document.querySelector('.temperature__status');
+    this.value = document.querySelector('.temperature__value');
+    this.unit = document.querySelector('.temperature__unit');
+
+    rAFTimeout(() => this.animate(), 400);
   }
 
   render() {
