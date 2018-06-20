b5726f5a923fd3c47ce26b0ffa3af5935d4a2fc0
Ion D. Filho <iondrimba@gmail.com>
2018-06-19 21:09:19 -0300

    navigation animations

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import rAFTimeout from '../../helpers/rAFTimeout';
 import './index.css';
 
 class Navigation extends Component {
+  animate() {
+    rAFTimeout(() => {
+      this.dots.map((elment, index) => {
+        return rAFTimeout(() => {
+          elment.classList.add('animate-in')
+        }, index * 150);
+      });
+    }, 350);
+  }
+
+  componentDidMount() {
+    this.dots = [...document.querySelectorAll('.navigation__dot')];
+
+    rAFTimeout(() => this.animate(), 350);
+  }
+
   render() {
     return (
       <section className="navigation">
-        <div className="navigation-dot"></div>
-        <div className="navigation-dot"></div>
+        <div className="navigation__dot"></div>
+        <div className="navigation__dot"></div>
       </section>
     )
   }
