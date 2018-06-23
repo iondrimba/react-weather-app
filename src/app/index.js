8b88e193c0380757dc9d86a4c40da3a0c55290a6
Ion D. Filho <iondrimba@gmail.com>
2018-06-22 23:29:30 -0300

    loading data from mock

--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,10 @@ import initialState from '../initialState';
 import Home from './Home';
 import Loader from '../components/Loader';
 import rAFTimeout from '../helpers/rAFTimeout';
+import timeConvert from '../helpers/time';
+import icons from '../helpers/icons';
+import weekdays from '../helpers/weekdays';
+
 
 class App extends Component {
   constructor() {
@@ -19,26 +23,52 @@ class App extends Component {
     this.state = { ...initialState };
 
     this.loader = React.createRef();
-
-    this.init();
   }
 
   async init() {
+    rAFTimeout(() => this.loader.current.animateIn(), 100);
+
     await this.ipFetcher.fetch();
     await this.ipGeoLocation.fetch(this.ipFetcher.ip);
     await this.foreCastAPI.fetch(this.ipGeoLocation.data.latitude, this.ipGeoLocation.data.longitude);
-  }
-
-  componentDidMount() {
-    rAFTimeout(() => this.loader.current.animateIn(), 100);
 
     setTimeout(() => {
       this.loader.current.animateOut();
 
       setTimeout(() => {
-        //this.setState({ dataLoaded: true });
+        this.setState({ dataLoaded: true });
+        this.setState({
+          currentCondition: {
+            ...initialState, location: this.ipGeoLocation.data.city,
+            temperature: Math.round(this.foreCastAPI.data.currently.temperature),
+            weather: this.foreCastAPI.data.currently.summary
+          },
+          foreCastHourly: this.foreCastAPI.data.hourly.data.map((item) => {
+            return {
+              time: timeConvert(item.time).hours,
+              rainProbability: item.precipProbability,
+              temperature: Math.round(item.temperature),
+              icon: icons[item.icon].id
+            }
+          }).slice(1, 6),
+          foreCastDaily: this.foreCastAPI.data.daily.data.map((item) => {
+            return {
+              weekDay: weekdays(timeConvert(item.time).weekDay),
+              rainProbability: item.precipProbability,
+              icon: icons[item.icon].id,
+              temperature: {
+                max: Math.round(item.temperatureMax),
+                min: Math.round(item.temperatureMin)
+              }
+            }
+          }).slice(1, 6)
+        });
       }, 400);
     }, 1000);
+  }
+
+  componentDidMount() {
+    this.init();
   }
 
   render() {
