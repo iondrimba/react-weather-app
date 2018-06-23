6bbfb8fe04e488ec947bc51dfb937dd8410ee812
Ion D. Filho <iondrimba@gmail.com>
2018-06-22 21:10:14 -0300

    updated forecast api

--- src/api/foreCastAPI.js
+++ src/api/foreCastAPI.js
@@ -1,17 +1,16 @@
+import data from '../mock/ipSearch.json';
+
 class ForeCastAPI {
   constructor() {
     this.secret = process.env.REACT_APP_DARK_SKY_API_CODE;
-    this.endpoint = (latitude, longitude) => `https://api.darksky.net/forecast/${this.secret}/${latitude},${longitude}`;
+    this.endpoint = (latitude, longitude) => `https://weather-api-nodejs.herokuapp.com/api?latitude=${latitude}&longitude=${longitude}`;
     this.data = null;
   }
 
   async fetch(latitude, longitude) {
     try {
-      const response = await fetch(this.endpoint(latitude, longitude), {
-        mode: 'no-cors'
-      });
-      const result = await response.json();
-
+      const response = await fetch(this.endpoint(latitude, longitude));
+      this.data =  data;// await response.json();
     } catch (error) {
       console.log(error.message);
     }
