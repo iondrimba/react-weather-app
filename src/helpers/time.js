2b7a35a872d04b945d3b0f9501b173f57fd326a3
Ion D. Filho <iondrimba@gmail.com>
2018-06-22 23:28:52 -0300

    added time helper

---
+++ src/helpers/time.js
@@ -0,0 +1,25 @@
+const addLeadingZero = (value) => (value < 10) ? `0${value}` : value;
+
+export default function (unixTimestamp) {
+  const date = new Date(unixTimestamp * 1000);
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const weekDay = date.getDay();
+
+  month = addLeadingZero(month);
+  day = addLeadingZero(day);
+  hours = addLeadingZero(hours);
+  minutes = addLeadingZero(minutes);
+
+  return {
+    weekDay,
+    day,
+    month,
+    year,
+    hours,
+    minutes
+  };
+}
