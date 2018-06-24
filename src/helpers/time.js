4c36c93afefc7e377d53324797271b37d2e2d714
Ion D. Filho <iondrimba@gmail.com>
2018-06-24 20:21:54 -0300

    debug time

--- src/helpers/time.js
+++ src/helpers/time.js
@@ -2,6 +2,7 @@ const addLeadingZero = (value) => (value < 10) ? `0${value}` : value;
 
 export default function (unixTimestamp, locale = 'en-US') {
   const date = new Date(unixTimestamp * 1000);
+  console.log('date', new Date(unixTimestamp * 1000));
   const year = date.getFullYear();
   let month = date.getMonth() + 1;
   let day = date.getDate();
