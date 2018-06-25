3910c22e0934204038f6bc0a0e0812df125cfb30
Ion D. Filho <iondrimba@gmail.com>
2018-06-24 21:11:12 -0300

    fixed time spec

--- spec/helpers/time.spec.js
+++ spec/helpers/time.spec.js
@@ -3,10 +3,10 @@ import time from '../../src/helpers/time';
 const unixTimeStamp = 1529716706;
 
 describe('Time', () => {
-  it('matches localeDateString with Friday, June 22, 2018', () => {
+  it('matches localeDateString with Saturday, June 23, 2018', () => {
     const result = time(unixTimeStamp);
 
-    expect(result.localeDateString).toEqual('Friday, June 22, 2018');
+    expect(result.localeDateString).toEqual('Saturday, June 23, 2018');
   });
 
   it('matches weekDay with 6', () => {
@@ -33,10 +33,10 @@ describe('Time', () => {
     expect(result.year).toEqual(2018);
   });
 
-  it('matches hours with 01', () => {
+  it('matches hours with 04', () => {
     const result = time(unixTimeStamp);
 
-    expect(result.hours).toEqual('01');
+    expect(result.hours).toEqual('04');
   });
 
   it('matches minutes with 18', () => {
--- src/helpers/time.js
+++ src/helpers/time.js
@@ -2,21 +2,22 @@ const addLeadingZero = (value) => (value < 10) ? `0${value}` : value;
 
 export default function (unixTimestamp, locale = 'en-US') {
   const date = new Date(unixTimestamp * 1000);
-  console.log('date', date);
-  console.log('date', date.getTimezoneOffset());
-  const year = date.getUTCFullYear();
-  let month = date.getUTCMonth() + 1;
-  let day = date.getUTCDate();
-  let hours = date.getUTCHours();
-  let minutes = date.getUTCMinutes();
-  const weekDay = date.getUTCDay();
+  const utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+  const finalDate = new Date(utc);
+
+  const year = finalDate.getUTCFullYear();
+  let month = finalDate.getUTCMonth() + 1;
+  let day = finalDate.getUTCDate();
+  let hours = finalDate.getUTCHours();
+  let minutes = finalDate.getUTCMinutes();
+  const weekDay = finalDate.getUTCDay();
 
   month = addLeadingZero(month);
   day = addLeadingZero(day);
   hours = addLeadingZero(hours);
   minutes = addLeadingZero(minutes);
 
-  const localeDateString = date.toLocaleDateString(locale, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  const localeDateString = finalDate.toLocaleDateString(locale, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
   return {
     localeDateString,
