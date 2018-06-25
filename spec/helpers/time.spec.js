112e4fdc5db0d0f89207124071ee8836dccaeaac
Ion D. Filho <iondrimba@gmail.com>
2018-06-24 22:11:16 -0300

    fixing time spec

--- spec/helpers/time.spec.js
+++ spec/helpers/time.spec.js
@@ -3,22 +3,22 @@ import time from '../../src/helpers/time';
 const unixTimeStamp = 1529886488;
 
 describe('Time', () => {
-  it('matches localeDateString with Saturday, June 23, 2018', () => {
+  it('matches localeDateString with Monday, June 25, 2018', () => {
     const result = time(unixTimeStamp);
 
-    expect(result.localeDateString).toEqual('Saturday, June 23, 2018');
+    expect(result.localeDateString).toEqual('Monday, June 25, 2018');
   });
 
-  it('matches weekDay with 6', () => {
+  it('matches weekDay with 1', () => {
     const result = time(unixTimeStamp);
 
-    expect(result.weekDay).toEqual(6);
+    expect(result.weekDay).toEqual(1);
   });
 
-  it('matches day with 23', () => {
+  it('matches day with 25', () => {
     const result = time(unixTimeStamp);
 
-    expect(result.day).toEqual(23);
+    expect(result.day).toEqual(25);
   });
 
   it('matches month with "06"', () => {
@@ -33,15 +33,15 @@ describe('Time', () => {
     expect(result.year).toEqual(2018);
   });
 
-  it('matches hours with 07', () => {
+  it('matches hours with 00', () => {
     const result = time(unixTimeStamp);
 
-    expect(result.hours).toEqual('07');
+    expect(result.hours).toEqual('00');
   });
 
-  it('matches minutes with 18', () => {
+  it('matches minutes with 28', () => {
     const result = time(unixTimeStamp);
 
-    expect(result.minutes).toEqual(18);
+    expect(result.minutes).toEqual(28);
   });
 });
--- src/helpers/time.js
+++ src/helpers/time.js
@@ -1,31 +1,23 @@
-import getTime from 'date-fns/get_time'
-
 const addLeadingZero = (value) => (value < 10) ? `0${value}` : value;
 
 export default function (unixTimestamp, locale = 'en-US') {
   const date = new Date(unixTimestamp * 1000);
-  const utc = getTime(date) + (date.getTimezoneOffset() * 60000);
+  const utc = date.getTime() + date.getTimezoneOffset() * 60000;
   const finalDate = new Date(utc);
 
-  console.log('date', date);
-  console.log('new Date', new Date());
-  console.log('finalDate', finalDate);
-
-  // finalDate.setUTCHours(finalDate.getUTCHours() + (date.getTimezoneOffset() / 60))
-
-  const year = finalDate.getUTCFullYear();
-  let month = finalDate.getUTCMonth() + 1;
-  let day = finalDate.getUTCDate();
-  let hours = finalDate.getUTCHours();
-  let minutes = finalDate.getUTCMinutes();
-  const weekDay = finalDate.getUTCDay();
+  const year = finalDate.getFullYear();
+  let month = finalDate.getMonth() + 1;
+  let day = finalDate.getDate();
+  let hours = finalDate.getHours();
+  let minutes = finalDate.getMinutes();
+  const weekDay = finalDate.getDay();
 
   month = addLeadingZero(month);
   day = addLeadingZero(day);
   hours = addLeadingZero(hours);
   minutes = addLeadingZero(minutes);
 
-  const localeDateString = finalDate.toLocaleDateString(locale, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  const localeDateString = finalDate.toLocaleDateString(locale, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });;
 
   return {
     localeDateString,
