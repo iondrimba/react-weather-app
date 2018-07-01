8d5ccd3394ea7840bb042d4c46ff982ef61e69d9
Ion D. Filho <iondrimba@gmail.com>
2018-07-01 18:08:25 -0300

    fixed spec

--- spec/components/DateCurrent/index.spec.js
+++ spec/components/DateCurrent/index.spec.js
@@ -6,14 +6,14 @@ enzymeConfig();
 
 describe('DateCurrent', () => {
   it('matches snapshot', () => {
-    const component = mount(<DateCurrent />);
+    const component = mount(<DateCurrent date='Sunday, July 1, 2018'/>);
 
     expect(component).toMatchSnapshot();
   });
 
   describe('componentDidMount', () => {
     it('calls animate', (done) => {
-      const component = mount(<DateCurrent />);
+      const component = mount(<DateCurrent date='Sunday, July 1, 2018'/>);
       const instance = component.instance();
 
       instance.animate = jest.fn();
