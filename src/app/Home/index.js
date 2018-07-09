86b82e7e47e99b46de0b9ce649db603876b83647
Ion D. Filho <iondrimba@gmail.com>
2018-07-09 13:56:47 -0300

    added swipe gesture

--- src/app/Home/index.js
+++ src/app/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import Drag from '../../helpers/drag';
+import Swiper from 'swiper';
 import ForeCast from '../../components/Forecast';
 import ForeCastTemperature from '../../components/Forecast/Temperature';
 import Location from '../../components/Location';
@@ -9,7 +9,6 @@ import RainProbability from '../../components/RainProbability';
 import GPSLocation from '../../components/GPSLocation';
 import Info from '../../components/Info';
 import DateCurrent from '../../components/DateCurrent';
-import rAFTimeout from '../../helpers/rAFTimeout';
 import PropTypes from 'prop-types';
 
 class Home extends Component {
@@ -17,25 +16,12 @@ class Home extends Component {
     super();
 
     this.state = {
-      currentForecast: 'hourly'
+      currentForecast: 'hourly',
+      forecastIndex: ['hourly', 'daily'],
     };
 
-    this.drag = new Drag();
     this.onInfoClick = this.onInfoClick.bind(this);
     this.onInfoClose = this.onInfoClose.bind(this);
-  }
-
-  animateForecastSection(forecasts, callback) {
-    forecasts.map(item => rAFTimeout(() => callback(item), 300));
-  }
-
-  onDragRightStart() {
-    this.animateForecastSection(this.forecasts, (item) => {
-      item.classList.remove('move-right');
-      item.classList.add('move-left');
-
-      this.setState({ currentForecast: 'hourly' });
-    });
   }
 
   onInfoClick() {
@@ -46,21 +32,17 @@ class Home extends Component {
     this.props.onInfoClose();
   }
 
-  onDragLeftStart() {
-    this.animateForecastSection(this.forecasts, (item) => {
-      item.classList.remove('move-left');
-      item.classList.add('move-right');
-
-      this.setState({ currentForecast: 'daily' });
-    });
-  }
-
   componentDidMount() {
     this.forecasts = [...document.querySelectorAll('.forecasts__period')];
 
-    this.drag.setup(document.getElementsByClassName('forecasts__scroll-panel')[0]);
+    this.swiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      loop: false
+    });
 
-    this.drag.onDragStart(this.onDragRightStart.bind(this), this.onDragLeftStart.bind(this));
+    this.swiper.on('slideChangeTransitionEnd', () => {
+      this.setState({ currentForecast: this.state.forecastIndex[this.swiper.activeIndex] });
+    });
   }
 
   render() {
@@ -74,27 +56,29 @@ class Home extends Component {
       <section className="forecasts">
         <Navigation currentForecast={this.state.currentForecast} />
 
-        <div className="forecasts__scroll-panel">
-          <section className="forecasts__period">
-            {
-              this.props.foreCastHourly.map((item, index) => <ForeCast key={`hourly-${index}`} id={`hourly-${index}`} title={`${item.time}:00`} icon={item.icon} animationDelay={index}>
-                <ForeCastTemperature temperature={item.temperature} />
-                <RainProbability probability={item.rainProbability} />
-              </ForeCast>)
-            }
-          </section>
+        <div className="forecasts__scroll-panel swiper-container">
+          <div className="swiper-wrapper">
+            <section className="forecasts__period swiper-slide">
+              {
+                this.props.foreCastHourly.map((item, index) => <ForeCast key={`hourly-${index}`} id={`hourly-${index}`} title={`${item.time}:00`} icon={item.icon} animationDelay={index}>
+                  <ForeCastTemperature temperature={item.temperature} />
+                  <RainProbability probability={item.rainProbability} />
+                </ForeCast>)
+              }
+            </section>
 
-          <section className="forecasts__period forecasts--daily">
-            {
-              this.props.foreCastDaily.map((item, index) => <ForeCast key={`daily-${index}`} id={`daily-${index}`} title={item.weekDay} icon={item.icon} animationDelay={index}>
-                <div>
-                  <ForeCastTemperature temperature={item.temperature.max} />
-                  <ForeCastTemperature temperature={item.temperature.min} />
-                </div>
-                <RainProbability probability={item.rainProbability} />
-              </ForeCast>)
-            }
-          </section>
+            <section className="forecasts__period forecasts--daily swiper-slide">
+              {
+                this.props.foreCastDaily.map((item, index) => <ForeCast key={`daily-${index}`} id={`daily-${index}`} title={item.weekDay} icon={item.icon} animationDelay={index}>
+                  <div>
+                    <ForeCastTemperature temperature={item.temperature.max} />
+                    <ForeCastTemperature temperature={item.temperature.min} />
+                  </div>
+                  <RainProbability probability={item.rainProbability} />
+                </ForeCast>)
+              }
+            </section>
+          </div>
         </div>
       </section>
     </Fragment>
