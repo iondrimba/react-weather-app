cd4469ace2bf834141f1e049c13f8f3b6566a4b9
Ion D. Filho <iondrimba@gmail.com>
2018-07-23 22:19:32 -0300

    extracted stateless ui

--- src/components/Close/index.js
+++ src/components/Close/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import rAFTimeout from '../../helpers/rAFTimeout';
-import './index.scss';
+import UIClose from './ui'
 
 class Close extends Component {
   constructor() {
@@ -25,11 +25,7 @@ class Close extends Component {
 
   render() {
     return (
-      <button ref={this.button} type="button" className="button-close" aria-label="Close view" onClick={this.onClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
-          <path d="M18 6L6 18M6 6l12 12" />
-        </svg>
-      </button>
+      <UIClose ref={this.button} onClick={this.onClick} />
     )
   }
 }
