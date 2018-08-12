01367c10c57e6ce6bdee634ac2e1ed56c10bb9ad
Ion D. Filho <iondrimba@gmail.com>
2018-08-12 18:56:01 -0300

    added animation

--- src/components/Refresh/index.js
+++ src/components/Refresh/index.js
@@ -7,28 +7,33 @@ class Refresh extends Component {
   constructor() {
     super();
 
-    this.button = React.createRef();
-    this.onClick = this.onClick.bind(this);
+    this.view = React.createRef();
   }
 
-  // animate() {
-  //   rAFTimeout(() => this.button.current.classList.add('animate-in'), 1);
-  // }
+  animate() {
+    rAFTimeout(() => this.view.current.classList.add('animate-in'), 350);
+  }
 
-  // hide() {
-  //   rAFTimeout(() => this.button.current.classList.remove('animate-in'), 1);
-  // }
+  componentDidMount() {
+    rAFTimeout(() => this.animate(), 350);
+  }
 
-  onClick() {
-    this.props.onClick();
+  getClass({ updating }) {
+    let animation = '';
+
+    if (this.view.current) {
+      animation = 'animate-in';
+    }
+
+    return `refresh ${animation} ${updating ? 'fetching' : ''}`;
   }
 
   render() {
     return (
-      <section className={`refresh ${this.props.updating ? 'fetching': ''}`}>
+      <section ref={this.view} className={`${this.getClass(this.props)}`}>
         <span className="refresh__time">{this.props.time}</span>
-        <button ref={this.button} type="button" className="button-refresh" aria-label="Refresh content" onClick={this.onClick}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
+        <button type="button" className="button-refresh" aria-label="Refresh content" onClick={this.props.onClick}>
+          <svg viewBox="0 0 96 96">
             <path d="M48 15.5c-8.8 0-17 3.5-23 9.5l-6.3-6.3v19.5h19.5l-8.6-8.6C34.4 24.8 40.9 22 48 22c14.3 0 26 11.7 26 26S62.3 74 48 74 22 62.3 22 48h-6.5c0 17.9 14.6 32.5 32.5 32.5S80.5 65.9 80.5 48 65.9 15.5 48 15.5z" />
           </svg>
         </button>
