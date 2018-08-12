d0c0e89336b9f4345750b7f66b3292904f170084
Ion D. Filho <iondrimba@gmail.com>
2018-08-12 16:44:18 -0300

    extracted data fetching

--- src/app/index.js
+++ src/app/index.js
@@ -1,91 +1,53 @@
 import React, { Component, Fragment } from 'react';
-import IpGeoLocation from '../api/ipGeoLocation';
-import ForeCastAPI from '../api/foreCastAPI';
-import ReverseGeoLocation from '../api/reverseGeoLocation';
-import initialState from '../initialState';
 import Home from './Home';
 import Info from './Info';
 import Loader from '../components/Loader';
-import IpFetcher from '../api/ipfetcher';
 import rAFTimeout from '../helpers/rAFTimeout';
-import timeConvert from '../helpers/time';
-import icons from '../helpers/icons';
-import weekdays from '../helpers/weekdays';
+import Storage from './storage';
 import './index.scss';
 
 class App extends Component {
   constructor() {
     super();
 
-    this.ipFetcher = new IpFetcher();
-    this.ipGeoLocation = new IpGeoLocation();
-    this.foreCastAPI = new ForeCastAPI(process.env.REACT_APP_DARK_SKY_API_CODE);
-    this.reverseGeoLocation = new ReverseGeoLocation();
-    this.onGetCurrentLocation = this.onGetCurrentLocation.bind(this);
-
-    this.state = { ...initialState };
-
     this.loader = React.createRef();
     this.onInfoClick = this.onInfoClick.bind(this);
     this.onInfoClose = this.onInfoClose.bind(this);
     this.onRefreshClick = this.onRefreshClick.bind(this);
+    this.onGetCurrentLocation = this.onGetCurrentLocation.bind(this);
+
+    this.storage = new Storage(process.env.REACT_APP_DARK_SKY_API_CODE);
+    this.state = { ...this.storage.data };
   }
 
   async init() {
     rAFTimeout(() => this.loader.current.animateIn(), 100);
 
-    await this.ipFetcher.fetch();
-    await this.ipGeoLocation.fetch(this.ipFetcher.ip);
-    await this.foreCastAPI.fetch(this.ipGeoLocation.data.latitude, this.ipGeoLocation.data.longitude);
+    await this.storage.fetch();
 
     rAFTimeout(() => {
       this.loader.current.animateOut();
 
-      rAFTimeout(() => this.updatedState(), 600);
+      rAFTimeout(() => this.updatedState(this.storage.data), 600);
     }, 1000);
   }
 
-  updatedState() {
+  updatedState(data) {
     this.setState({
+      ...data,
       showInfo: false,
       dataLoaded: true,
-      latitude: this.foreCastAPI.data.latitude,
-      longitude: this.foreCastAPI.data.longitude,
-      currentCondition: {
-        ...initialState,
-        location: this.ipGeoLocation.data.city,
-        date: timeConvert(this.foreCastAPI.data.currently.time).localeDateString,
-        temperature: Math.round(this.foreCastAPI.data.currently.temperature),
-        weather: this.foreCastAPI.data.currently.summary
-      },
-      foreCastHourly: this.foreCastAPI.data.hourly.data.slice(0, 5).map((item) => ({
-        time: timeConvert(item.time).hours,
-        rainProbability: Math.round(item.precipProbability * 100),
-        temperature: Math.round(item.temperature),
-        icon: icons(item.icon).id
-      })),
-      foreCastDaily: this.foreCastAPI.data.daily.data.slice(1, 6).map(item => ({
-        weekDay: weekdays(timeConvert(item.time).weekDay),
-        rainProbability: Math.round(item.precipProbability * 100),
-        icon: icons(item.icon).id,
-        temperature: {
-          max: Math.round(item.temperatureMax),
-          min: Math.round(item.temperatureMin)
-        }
-      }))
     });
   }
 
   async onGetCurrentLocation({ latitude, longitude }) {
-    await this.reverseGeoLocation.fetch(latitude, longitude);
-    await this.foreCastAPI.fetch(latitude, longitude);
-    this.ipGeoLocation.data.city = this.reverseGeoLocation.data[0].city || this.reverseGeoLocation.data[0].state;
+    this.storage.getLocation(latitude, longitude);
 
-    this.updatedState();
+    this.updatedState(this.storage.data);
   }
 
   onRefreshClick() {
-    const { latitude, longitude } = this.foreCastAPI.data;
+    const { latitude, longitude } = this.storage.data;
 
     this.onGetCurrentLocation({ latitude, longitude });
   }
