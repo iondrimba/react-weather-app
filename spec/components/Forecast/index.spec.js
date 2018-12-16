ee284d8fc0ed080f80740377c25a4e43748234c2
Ion D. Filho <iondrimba@gmail.com>
2018-12-16 10:27:57 -0200

    fix forecast compononent spec

--- spec/components/Forecast/index.spec.js
+++ spec/components/Forecast/index.spec.js
@@ -4,7 +4,7 @@ import { enzymeConfig, mount } from '../../enzymeConfig';
 
 enzymeConfig();
 
-const ForecastComponent = (<Forecast id='11' title='some title' icon='xpto' animationDelay={1} > <h1>Children</h1> </Forecast>);
+const ForecastComponent = (<Forecast id='11' title='some title' icon='wi-cloudy' animationDelay={1} > <h1>Children</h1> </Forecast>);
 
 describe('Forecast', () => {
   it('matches snapshot', () => {
@@ -28,10 +28,12 @@ describe('Forecast', () => {
   });
 
   describe('.icon', () => {
-    it('matches source icon path svg/xpto.svg', () => {
+    it('matches source icon path wi-cloudy.svg', () => {
       const component = mount(ForecastComponent);
 
-      expect(component.find('[src="svg/xpto.svg"]').length).toEqual(1);
+      component.debug();
+
+      expect(component.find('[src="wi-cloudy.svg"]').length).toEqual(1);
     });
   });
 });
