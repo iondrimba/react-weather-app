682ceb83c48e0fe873daee5948b5dd8df1b9456a
Ion D. Filho <iondrimba@gmail.com>
2019-01-04 19:21:32 -0200

    replace react.memo with a pure component

--- src/components/RainProbability/index.js
+++ src/components/RainProbability/index.js
@@ -1,15 +1,18 @@
-import React, { memo } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import svg from '../../svg/rain-probality.svg';
 import './index.scss';
 
-// eslint-disable-next-line react/display-name
-const RainProbability = memo(({ probability }) => (
-  <div className="rain">
-    <img width="24" height="24" className="rain__icon" src={svg} alt={`rain probability ${probability}`} />
-    <span className="rain__probability">{`${probability}%`}</span>
-  </div>
-));
+class RainProbability extends PureComponent {
+  render() {
+    return (
+      <div className="rain">
+        <img width="24" height="24" className="rain__icon" src={svg} alt={`rain probability ${this.props.probability}`} />
+        <span className="rain__probability">{`${this.props.probability}%`}</span>
+      </div>
+    )
+  }
+}
 
 RainProbability.propTypes = {
   probability: PropTypes.number.isRequired
