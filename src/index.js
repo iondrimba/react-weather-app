68c58532d022fec21ce26faad7c13ad0c2e620a0
Ion Drimba Filho <iondrimba@gmail.com>
2021-07-07 13:39:12 -0300

    Fix service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
+import { register } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+register();
